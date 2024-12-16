96c0ed4cde40297dd36cebb4e305f8a5e0ec6526
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-12-16 15:24:19 +0100

    initial commit

---
+++ src/test/java/org/carlosramosdev/curso/springboot/items/ItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlosramosdev.curso.springboot.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
