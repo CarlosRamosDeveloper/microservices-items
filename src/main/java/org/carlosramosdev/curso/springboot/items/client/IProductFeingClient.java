accb746520c36c8defe99707c654c9c172995bb1
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-12-16 16:42:31 +0100

    Created IProductFeignClient interface

---
+++ src/main/java/org/carlosramosdev/curso/springboot/items/client/IProductFeingClient.java
@@ -0,0 +1,18 @@
+package org.carlosramosdev.curso.springboot.items.client;
+
+import org.carlosramosdev.curso.springboot.items.models.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(url="localhost:8001")
+public interface IProductFeingClient {
+
+    @GetMapping
+    List<Product> list();
+
+    @GetMapping("/{id}")
+    Product findById(@PathVariable Long id);
+}
