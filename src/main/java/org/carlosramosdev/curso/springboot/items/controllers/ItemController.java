acfda6eb32ecefbfe74ac2869e7f745e2f93bafe
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-12-16 17:13:44 +0100

    created ItemController

---
+++ src/main/java/org/carlosramosdev/curso/springboot/items/controllers/ItemController.java
@@ -0,0 +1,32 @@
+package org.carlosramosdev.curso.springboot.items.controllers;
+
+import org.carlosramosdev.curso.springboot.items.models.Item;
+import org.carlosramosdev.curso.springboot.items.services.IItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ItemController {
+
+    private final IItemService service;
+
+    public ItemController(IItemService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public List<Item> list(){
+        return service.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Item> findById(@PathVariable long id){
+        Optional<Item> item = service.findById(id);
+        return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
